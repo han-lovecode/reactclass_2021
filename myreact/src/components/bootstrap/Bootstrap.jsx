3b7599e0a16a534de78427dfeb547ca9e9220b16
han-lovecode <mcdreamy531@gmail.com>
2022-01-05 21:52:36 +0900

    Day10
    Carousel
    Dropdown

--- myreact/src/components/bootstrap/Bootstrap.jsx
+++ myreact/src/components/bootstrap/Bootstrap.jsx
@@ -1,11 +1,16 @@
 import Accordion from "./Accordion";
 import { accordionData } from "../../data";
+import { carouselData } from "../../data";
+import Dropdown from "./Dropdown";
+import Carousel from "./Carousel";
 
 const Bootstrap = () => {
   return (
-    <>
+    <div style={{ paddingLeft: 200 }}>
       <Accordion data={accordionData} />
-    </>
+      <Dropdown />
+      <Carousel data={carouselData} />
+    </div>
   );
 };
 
--- myreact/src/data/index.js
+++ myreact/src/data/index.js
@@ -24,3 +24,10 @@ export const accordionData = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis",
   },
 ];
+
+export const carouselData = [
+  "https://i.pinimg.com/236x/fb/24/5a/fb245a6b5b414039dadefc112cca69b4.jpg",
+  "https://cdn.crowdpic.net/list-thumb/thumb_l_B7DC708C50FC16DE455BE175B23D6D36.jpg",
+  "https://og-data.s3.amazonaws.com/media/artworks/half/A0880/A0880-0016.jpg",
+  "http://ojsfile.ohmynews.com/STD_IMG_FILE/2016/0516/IE001963941_STD.jpg",
+];
